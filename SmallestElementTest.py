5e687a764c32cdb7479a8bca360021bde32ddf62
adriana <adrianapenate98@gmail.com>
2021-02-15 12:17:36 +0000

    Green test - test_should_give_smallest_numbers

--- SmallestElementTest.py
+++ SmallestElementTest.py
@@ -11,13 +11,18 @@ class SmallestElement:
             if self.pass_limit(self, element, limit) and len(smallest_elements) < limit:
                 smallest_elements.append(element)
             elif self.pass_limit(self, element, limit) and max(smallest_elements) > element:
-                smallest_elements.pop(smallest_elements.index(max(smallest_elements)))
+                smallest_elements.pop(SmallestElement.get_biggest_elem_index(smallest_elements))
                 smallest_elements.append(element)
 
         return smallest_elements
 
+    @classmethod
+    def get_biggest_elem_index(cls, smallest_elements):
+        return smallest_elements.index(max(smallest_elements))
+
     def pass_limit(self, element, limit):
         return element <= limit
+
 
 class FindPairsTest(unittest.TestCase):
 
