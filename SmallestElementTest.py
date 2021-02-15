43824e0c7ab262ba3acde1231d827b3315e28cfc
adriana <adrianapenate98@gmail.com>
2021-02-15 11:48:23 +0000

    Red test - test_should_give_one_number

--- SmallestElementTest.py
+++ SmallestElementTest.py
@@ -12,6 +12,10 @@ class FindPairsTest(unittest.TestCase):
     def test_should_skip_search(self):
         self.assertEqual([], SmallestElement.first_n_smallest([1, 2, 3, 4, 5], 0))
 
+    def test_should_give_one_number(self):
+        self.assertEqual([1], SmallestElement.first_n_smallest([1], 1))
+
+
 
 if __name__ == '__main__':
     unittest.main()
