fa59fb40393434229b95df4ac2131ccc4182d059
adriana <adrianapenate98@gmail.com>
2021-02-15 13:29:04 +0000

    Green test - test_should_work_with_random_numbers

--- SmallestElement.py
+++ SmallestElement.py
@@ -17,8 +17,14 @@ def add_element_to(smallest_elements, element):
 
 
 def remove_biggest_elem(smallest_elements):
-    smallest_elements.pop(get_biggest_elem_index(smallest_elements))
+    if smallest_elements.count(max(smallest_elements)) > 1:
+        smallest_elements.reverse()
+        smallest_elements.pop(get_biggest_elem_index(smallest_elements))
+        smallest_elements.reverse()
+    else:
+        smallest_elements.pop(get_biggest_elem_index(smallest_elements))
 
 
 def get_biggest_elem_index(smallest_elements):
     return smallest_elements.index(max(smallest_elements))
+
