c2ab68865a544f440f2774260d7ce1d59b274415
adriana <adrianapenate98@gmail.com>
2021-02-15 12:40:19 +0000

    Refactor - Class removed, code extracted as methods

--- SmallestElement.py
+++ SmallestElement.py
@@ -1,15 +1,24 @@
-class SmallestElement:
+def first_n_smallest(elements, limit):
+    if limit == 0:
+        return []
+    smallest_elements = []
+    for element in elements:
+        if element <= limit and len(smallest_elements) < limit:
+            add_element_to(smallest_elements, element)
+        elif element <= limit and max(smallest_elements) > element:
+            remove_biggest_elem(smallest_elements)
+            add_element_to(smallest_elements, element)
 
-    def first_n_smallest(self, elements, limit):
-        if limit == 0:
-            return []
-        smallest_elements = []
-        for element in elements:
-            if element <= limit and len(smallest_elements) < limit:
-                smallest_elements.append(element)
-            elif element <= limit and max(smallest_elements) > element:
-                smallest_elements.pop(smallest_elements.index(max(smallest_elements)))
-                smallest_elements.append(element)
+    return smallest_elements
 
-        return smallest_elements
 
+def add_element_to(smallest_elements, element):
+    smallest_elements.append(element)
+
+
+def remove_biggest_elem(smallest_elements):
+    smallest_elements.pop(get_biggest_elem_index(smallest_elements))
+
+
+def get_biggest_elem_index(smallest_elements):
+    return smallest_elements.index(max(smallest_elements))
--- SmallestElementTest.py
+++ SmallestElementTest.py
@@ -1,26 +1,26 @@
 import unittest
-from SmallestElement import SmallestElement
+from SmallestElement import first_n_smallest
 
 
 class FindPairsTest(unittest.TestCase):
 
     def test_should_skip_search(self):
-        self.assertEqual([], SmallestElement().first_n_smallest([1, 2, 3, 4, 5], 0))
+        self.assertEqual([], first_n_smallest([1, 2, 3, 4, 5], 0))
 
     def test_should_give_one_number(self):
-        self.assertEqual([1], SmallestElement().first_n_smallest([1], 1))
+        self.assertEqual([1], first_n_smallest([1], 1))
 
     def test_should_give_first_number(self):
-        self.assertEqual([1], SmallestElement().first_n_smallest([1, 2], 1))
+        self.assertEqual([1], first_n_smallest([1, 2], 1))
 
     def test_should_find_second_number(self):
-        self.assertEqual([1], SmallestElement().first_n_smallest([2, 1], 1))
+        self.assertEqual([1], first_n_smallest([2, 1], 1))
 
     def test_should_give_two_numbers(self):
-        self.assertEqual([1, 1], SmallestElement().first_n_smallest([1, 1, 1, 1], 2))
+        self.assertEqual([1, 1], first_n_smallest([1, 1, 1, 1], 2))
 
     def test_should_give_smallest_numbers(self):
-        self.assertEqual([1, 1], SmallestElement().first_n_smallest([4, 1, 2, 1], 2))
+        self.assertEqual([1, 1], first_n_smallest([4, 1, 2, 1], 2))
 
 
 if __name__ == '__main__':
