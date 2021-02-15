2305e7acea62a1a8d1d2fb0378cc59326982dff3
adriana <adrianapenate98@gmail.com>
2021-02-15 11:44:44 +0000

    Red test - test_should_skip_search

---
+++ SmallestElementTest.py
@@ -0,0 +1,17 @@
+import unittest
+
+
+class SmallestElement:
+    @classmethod
+    def first_n_smallest(cls, elements, limit):
+        pass
+
+
+class FindPairsTest(unittest.TestCase):
+
+    def test_should_skip_search(self):
+        self.assertEqual([], SmallestElement.first_n_smallest([1, 2, 3, 4, 5], 0))
+
+
+if __name__ == '__main__':
+    unittest.main()
